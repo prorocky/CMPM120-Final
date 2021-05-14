84affa7d2c11b6d0bf2e6e6dcbd3d40788437141
oshadian <oshadian@ucsc.edu>
2021-05-14 15:48:55 -0700

    added assets folder

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,6 +5,11 @@ class Play extends Phaser.Scene {
 
     preload() {
         // load images/sprites
-        this.load.image('starfield', 'assets/starfield.png');
+        this.load.image('main_room', 'assets/img/main_hub.png');
+    }
+
+    create() {
+        // background for room
+        this.background = this.add.tileSprite(0, 0, 1080, 1080, 'main_room').setOrigin(0, 0);
     }
 }
