84496b0ac3e449ce7ba5621d0453d81f7b33d3d4
oshadian <oshadian@ucsc.edu>
2021-05-24 05:47:41 -0700

    working on puzzle1, added music

--- src/scenes/Puzzle1.js
+++ src/scenes/Puzzle1.js
@@ -6,7 +6,10 @@ class Puzzle1 extends Phaser.Scene {
     preload() {
         // load images/sprites
         this.load.image('main_room1', 'assets/img/puzzle_room1.png');
-        this.load.image('p1', 'assets/img/toad.jpg');
+
+        // load audio
+        this.load.audio('greeting', 'assets/aud/WhoDoWeHave.wav');
+        this.load.audio('riddle', 'assets/aud/Riddle_Scene1.wav');
     }
 
     create() {
@@ -18,6 +21,15 @@ class Puzzle1 extends Phaser.Scene {
         
         //creating player
         player = this.physics.add.sprite(200, config.height -500, "p1");
+
+        // fade into scene
+        this.cameras.main.fadeIn(1000, 0, 0, 0);
+
+        // flag for playing riddle
+        this.playing = false;
+
+        // play greeting
+        this.sound.play('greeting');
 
         /*creating animations/linking them with movement 
         so that its a different animation depending on what direction its going in */
@@ -47,41 +59,63 @@ class Puzzle1 extends Phaser.Scene {
 
         this.door = this.physics.add.sprite(config.width - config.width, config.height / 2, 'door');
         
+        
         let door =this.add.existing(this.door);
        
         door.body.setCollideWorldBounds = true;
         
         //creating collsion detector
         this.physics.add.overlap(player, door, this.hitDoor1, null, this);
+
+        // temporary to see coords of player
+        this.coord = this.add.text(80, 80, 'X: ' + player.x + ' Y: ' + player.y);
     }
 
     update() {
 
         player.setVelocity(0,0);
+        this.coord.text = 'X: ' + Math.floor(player.x) + ' Y: ' + Math.floor(player.y);
         
         if (this.cursors.left.isDown) {
             //  Move to the left
             player.setVelocityX(-500);
             player.anims.play("left");
-          } else if (this.cursors.right.isDown) {
+        } else if (this.cursors.right.isDown) {
             //  Move to the right
             player.setVelocityX(500);
             player.anims.play("right");
-          }
+        }
     
-          if (this.cursors.up.isDown) {
+        if (this.cursors.up.isDown) {
             //  Move up
             player.setVelocityY(-500);
             player.anims.play("up");
-          } else if (this.cursors.down.isDown) {
+        } else if (this.cursors.down.isDown) {
             //  Move down
             player.setVelocityY(500);
             player.anims.play("down");
-          }
+        }
+
+        // press space to play riddle when near mirror
+        if (player.x > 400 && player.x < 600 && player.y < 400) {
+            this.input.keyboard.once('keydown-SPACE', () => {
+                if (!this.playing) {
+                    this.playRiddle();
+                }
+            })
+        }
     }
     
-//starts puzzle scene when objects collide 
-hitDoor1 (door,player){
-    this.scene.start('playScene');
-}
+    //starts puzzle scene when objects collide 
+    hitDoor1 (door, player){
+        this.scene.start('playScene');
+    }
+
+    playRiddle() {
+        this.playing = true;
+        this.sound.play('riddle');
+        this.time.delayedCall(7500, () => {
+            this.playing = false;
+        }, null, this);
+    }
 }
